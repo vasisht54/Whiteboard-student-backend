68b17e22c7fa57ded4bb6f4a877e45e0dcae4c8a
vasisht54 <vasisht.theviper@gmail.com>
2020-04-21 21:59:57 -0400

    Fixed port issues

--- server.js
+++ server.js
@@ -19,6 +19,11 @@ require('./controllers/questions.controller.server')(app);
 
 app.get('/', (req, res) => res.send("Hello there!"));
 
-app.listen(3000, () => {
+/*app.listen(3000, () => {
     console.log('Server is listening on port: 3000');
+});*/
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${ PORT }`);
 });
