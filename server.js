9f2a860d06c9bc423b3e0a5f5923d95acd049d2b
vasisht54 <vasisht.theviper@gmail.com>
2020-04-21 21:31:05 -0400

    Fixed cors bug

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
 
+app.use(function(req, res, next) {
+    const allowedOrigins = ['http://myherokuapp.com', 'http://localhost:4200', 'http://127.0.0.1:9000', 'http://localhost:9000'];
+    const origin = req.headers.origin;
+    if(allowedOrigins.indexOf(origin) > -1){
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
+    res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', true);
+    return next();
+});
+
 require('./controllers/quizzes.controller.server')(app);
 require('./controllers/questions.controller.server')(app);
 
